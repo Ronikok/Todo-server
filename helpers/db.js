6cbbdc914faa8a2e25d0dc8d4e461509c3200688
Ronikok <ronikokkonen@gmail.com>
2026-03-20 21:19:58 +0200

    debugging

--- helpers/db.js
+++ helpers/db.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
 const { Pool } = require('pg')
 
-const query = (sql, values = []) => {
+const query = (sql,values = []) => {
     return new Promise(async(resolve,reject) => {
         try {
             const pool = openDb()
             const result = await pool.query(sql,values)
             resolve(result)
         } catch(error){
-            reject(error.message)
+            reject(error)
         }
     })
 }
--- routes/todo.js
+++ routes/todo.js
@@ -11,7 +11,7 @@ try {
     res.status(200).json(rows)
     } catch (error) {
         console.log(error)
-        res.statusMessage = error
+        //res.statusMessage = error
         res.status(500).json({error: String(error)})
     }
 })
